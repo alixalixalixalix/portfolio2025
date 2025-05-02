057c00fcdb4687665874b80325b773cfa3f4e926
Alix Bocquier <alixbocqui@gmail.com>
2025-05-02 16:22:27 +0200

    +/- wip

--- src/components/xila/XilaListAffiche.js
+++ src/components/xila/XilaListAffiche.js
@@ -1,18 +1,25 @@
-import React from "react";
+import React, { useRef } from "react";
 import dataXilaAffiches from "../../data/xilaAffiches.json";
 
 const XilaListAffiche = () => {
+  const containerAffiche = useRef(null);
+  const affiches = containerAffiche.current.querySelectorAll("#containerAffiche img");
+
+  function bigger() {
+    affiches.style.width = "150px";
+  }
+
   return (
     <section id="xilaListAffiches" className="marginApp">
       <div className="xilaListAffichesButtons">
-        <div>
+        <div onClick={bigger}>
           <p>+</p>
         </div>
         <div>
           <p>-</p>
         </div>
       </div>
-      <div id="containerAffiche">
+      <div id="containerAffiche" ref={containerAffiche}>
         {dataXilaAffiches
           .sort((a, b) => b.id - a.id)
           .map((affiche) => {
--- src/components/xila/XilaListProjet.js
+++ src/components/xila/XilaListProjet.js
@@ -1,23 +1,49 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import dataXilaProjets from "../../data/xilaProjets.json";
 
-
 const XilaListProjet = () => {
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    const bodyWidth = document.body.clientWidth;
+    const bodyHeight = window.innerHeight;
+
+    const imgs = containerRef.current.querySelectorAll("img");
+
+    let loadedCount = 0;
+
+    imgs.forEach((img) => {
+      img.onload = () => {
+        loadedCount++;
+        if (loadedCount === imgs.length) {
+          // Toutes les images sont chargées
+          imgs.forEach((img) => {
+            const posX = Math.round(Math.random() * bodyWidth);
+            const posY = Math.round(Math.random() * bodyHeight);
+            img.style.position = "absolute";
+            img.style.left = `${posX}px`;
+            img.style.top = `${posY}px`;
+          });
+        }
+      };
+    });
+  }, []);
+
   return (
-    <div id="xilaListeProjet">
-      {dataXilaProjets
-          .map((projet) => {
-            const imgPath = require(`../../assets${projet.cover}`);
-            return <img key={projet.cover} src={imgPath} alt="" />;
-          })}
+    <div id="xilaListeProjet" ref={containerRef}>
+      {dataXilaProjets.map((projet) => {
+        const imgPath = require(`../../assets${projet.cover}`);
+        return <img key={projet.cover} src={imgPath} alt="" />;
+      })}
     </div>
   );
 };
 
 export default XilaListProjet;
 
-/*
-{dataXilaProjets.map((projet) => {
+
+    /*
+      {dataXilaProjets.map((projet) => {
         const imgPath = require(`../../assets${projet.cover}`);
         return (
           <div
@@ -26,4 +52,19 @@ export default XilaListProjet;
           ></div>
         );
       })}
-        */
+    */
+
+
+    /*
+      useEffect(() => {
+        const bodyWidth = document.body.clientWidth;
+        const bodyHeight = document.body.clientHeight;
+
+        document.querySelectorAll(".visages").forEach((visage) => {
+          const posX = Math.round(Math.random() * bodyWidth);
+          const posY = Math.round(Math.random() * bodyHeight);
+          visage.style.left = `${posX}px`;
+          visage.style.top = `${posY}px`;
+        });
+      }, []);
+    */
